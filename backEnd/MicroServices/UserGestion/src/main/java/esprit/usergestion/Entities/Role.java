11df2ae94863b894efa5b2604bee3a2e125e8fbf
achrafbannour1 <bannour.achraf@esprit.tn>
2025-04-10 16:23:14 +0100

    product baranche

---
+++ backEnd/MicroServices/UserGestion/src/main/java/esprit/usergestion/Entities/Role.java
@@ -0,0 +1,5 @@
+package esprit.usergestion.Entities;
+
+public enum Role {
+  CLIENT,ADMIN,MANAGER,EMPLOYEE
+}
--- backEnd/MicroServices/UserGestion/src/main/java/esprit/usergestion/UserGestionApplication.java
+++ backEnd/MicroServices/UserGestion/src/main/java/esprit/usergestion/UserGestionApplication.java
@@ -3,12 +3,14 @@ package esprit.usergestion;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class UserGestionApplication {
 
 	public static void main(String[] args) {
